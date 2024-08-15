9beec338bd2e95790712eda5509dd37529686788
Fauzi Arda <fauziardasaputra@gmail.com>
2024-08-15 17:08:40 +0700

    refactoring test and add test case for on selected option

--- QuizApp/QuestionViewController.swift
+++ QuizApp/QuestionViewController.swift
@@ -9,37 +9,51 @@ import Foundation
 import UIKit
 
 class QuestionViewController: UIViewController {
-    private var question: String = ""
-    private var options: [String] = []
+    private var question = String()
+    private var options = [String]()
+    private var selection: ((String) -> Void)? = nil
+    private let reuseIdentifier = "Cell"
     
     @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     
-    convenience init(question: String, options: [String]) {
+    convenience init(question: String, options: [String], selection: @escaping (String) -> Void = {_ in }) {
         self.init()
         self.question = question
         self.options = options
+        self.selection = selection
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
         headerLabel.text = question
         self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
 }
 
 
 extension QuestionViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        self.options.count
+        return options.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = dequeueCell(in: tableView)
         cell.textLabel?.text = self.options[indexPath.row]
         return cell
     }
     
-    
+    private func dequeueCell(in tableView: UITableView) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) {
+            return cell
+        }
+        return UITableViewCell(style: .default, reuseIdentifier: reuseIdentifier)
+    }
+}
+
+extension QuestionViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.selection?(options[indexPath.row])
+    }
 }
